432e9bdf77e1f41b9798a4e48adf664789539f77
isarpsiyahofficial <isarpsiyahofficial@gmail.com>
2026-08-05 10:51:02 +0300

    test: advance final-head registry through Persian

--- test/italian_final_head_test.dart
+++ test/italian_final_head_test.dart
@@ -2,7 +2,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:lefferion_prime_mizan/l10n/mizan_i18n.dart';
 
 void main() {
-  test('final Russian head exposes the complete thirteen-language runtime', () {
+  test('final Persian head exposes the complete sixteen-language runtime', () {
     expect(MizanI18n.supportedLanguageTags, {
       'tr',
       'en',
@@ -19,6 +19,7 @@ void main() {
       'ru',
       'uk',
       'ar',
+      'fa',
     });
     expect(MizanI18n.normalizeLanguageTag('it-IT'), 'it');
     expect(MizanI18n.normalizeLanguageTag('it_CH'), 'it');
@@ -33,5 +34,9 @@ void main() {
     expect(MizanI18n.isSupported('ru-RU'), isTrue);
     expect(MizanI18n.normalizeLanguageTag('ru_RU'), 'ru');
     expect(MizanI18n.isSupported('el-GR'), isTrue);
+    expect(MizanI18n.normalizeLanguageTag('fa-IR'), 'fa');
+    expect(MizanI18n.normalizeLanguageTag('fa_AF'), 'fa');
+    expect(MizanI18n.isSupported('fa'), isTrue);
+    expect(MizanI18n.isSupported('fa-IR'), isTrue);
   });
 }
