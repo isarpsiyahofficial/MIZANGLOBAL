32f30f6d979e4f73b88ac724d239a7f2228c03c6
isarpsiyahofficial <isarpsiyahofficial@gmail.com>
2026-08-07 13:25:47 +0300

    test(fil): lock exact 791-key parity

---
+++ test/filipino_static_key_parity_test.dart
@@ -0,0 +1,11 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:lefferion_prime_mizan/l10n/mizan_fil.dart';
+import 'package:lefferion_prime_mizan/l10n/mizan_id.dart';
+
+void main(){
+  test('Filipino owns all and only the 791 stable product keys',(){
+    expect(mizanFilipino,hasLength(791));
+    expect(mizanIndonesian,hasLength(791));
+    expect(mizanFilipino.keys.toSet(),mizanIndonesian.keys.toSet());
+  });
+}
