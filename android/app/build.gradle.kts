e42c09bc8c1b7307fa5a0c80c3ae9182f71ecf0d
isarpsiyahofficial <isarpsiyahofficial@gmail.com>
2026-08-15 12:14:43 +0300

    android: give MIZAN GLOBAL an independent package identity

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 android {
-    namespace = "com.lefferionprime.lefferion_prime_mizan"
+    namespace = "com.lefferionprime.mizanglobal"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -16,8 +16,7 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.lefferionprime.lefferion_prime_mizan"
+        applicationId = "com.lefferionprime.mizanglobal"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
