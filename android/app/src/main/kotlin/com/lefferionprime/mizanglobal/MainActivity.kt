0fb271fc56aec5ac9ed43496834c06999812f030
isarpsiyahofficial <isarpsiyahofficial@gmail.com>
2026-08-16 11:14:06 +0300

    add reinstall-stable Android device identity bridge

--- android/app/src/main/kotlin/com/lefferionprime/mizanglobal/MainActivity.kt
+++ android/app/src/main/kotlin/com/lefferionprime/mizanglobal/MainActivity.kt
@@ -1,5 +1,38 @@
 package com.lefferionprime.mizanglobal
 
+import android.provider.Settings
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+    private val deviceIdentityChannel =
+        "com.lefferionprime.mizanglobal/device_identity"
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        MethodChannel(
+            flutterEngine.dartExecutor.binaryMessenger,
+            deviceIdentityChannel,
+        ).setMethodCallHandler { call, result ->
+            when (call.method) {
+                "getAndroidId" -> {
+                    val androidId = Settings.Secure.getString(
+                        contentResolver,
+                        Settings.Secure.ANDROID_ID,
+                    )
+                    if (androidId.isNullOrBlank()) {
+                        result.error(
+                            "ANDROID_ID_UNAVAILABLE",
+                            "Android device identifier is unavailable.",
+                            null,
+                        )
+                    } else {
+                        result.success(androidId)
+                    }
+                }
+                else -> result.notImplemented()
+            }
+        }
+    }
+}
