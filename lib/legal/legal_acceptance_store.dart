6a2a7507522095b1cf1a936263523ad20a2c9c15
isarpsiyahofficial <isarpsiyahofficial@gmail.com>
2026-08-22 14:10:15 +0300

    fix: surface purchase terms persistence result

---
+++ lib/legal/legal_acceptance_store.dart
@@ -0,0 +1,38 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+import 'legal_documents.dart';
+
+abstract final class LegalAcceptanceStore {
+  static const String _generalAcceptanceKey = 'mizan_legal_acceptance_version';
+  static const String _purchaseAcceptanceKey = 'mizan_purchase_terms_version';
+
+  static String get currentVersion => MizanLegalDocuments.generalBundleVersion;
+  static String get currentPurchaseVersion =>
+      MizanLegalDocuments.purchaseTermsVersion;
+
+  static Future<bool> hasAcceptedCurrentLegalBundle() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getString(_generalAcceptanceKey) == currentVersion;
+  }
+
+  static Future<void> acceptCurrentLegalBundle() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString(_generalAcceptanceKey, currentVersion);
+  }
+
+  static Future<bool> hasAcceptedCurrentPurchaseTerms() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getString(_purchaseAcceptanceKey) == currentPurchaseVersion;
+  }
+
+  static Future<bool> acceptCurrentPurchaseTerms() async {
+    final prefs = await SharedPreferences.getInstance();
+    return await prefs.setString(_purchaseAcceptanceKey, currentPurchaseVersion);
+  }
+
+  static Future<void> clearForTest() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.remove(_generalAcceptanceKey);
+    await prefs.remove(_purchaseAcceptanceKey);
+  }
+}
